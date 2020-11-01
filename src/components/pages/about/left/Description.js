4e07225665ec7905e8d4dd2293dae4c5edb1458b
Jesse Pelzar <jessepelzar@gmail.com>
2020-11-01 18:54:55 -0500

    added favicon

--- src/components/pages/about/left/Description.js
+++ src/components/pages/about/left/Description.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export const Description = () => {
+    const descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     return (
         <div className="sm-container">
-            <p className="text-md-caladea">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p className="text-md-caladea">{descriptionText}</p>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: __dirname + '/src/index.html',
             filename: 'index.html',
-            inject: 'body'
+            inject: 'body',
+            favicon: "./src/favicon.ico"
         })
     ]
 };
